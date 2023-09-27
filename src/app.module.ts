2e963cb41e7ee44a8226de316c7afa5839503d2b
Matías Roy Rodriguez <matiasrr980@gmail.com>
2023-09-27 20:16:56 -0300

    feat: connect nest with mongoDB and add environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
-  controllers: [],
-  providers: [],
+	imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.MONGO_URI), AuthModule],
 })
-export class AppModule {}
+export class AppModule { }
