52eb94f1d57802d7dcf9c1b0534aa13becb6b21f
Matías Roy Rodriguez <matiasrr980@gmail.com>
2023-10-01 20:42:29 -0300

    feat: add user register and login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './entities/user.entity';
+import { Model } from 'mongoose';
+
+import * as bcryptjs from 'bcryptjs';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
-	create(createUserDto: CreateUserDto) {
-console.log(createUserDto);
-		return 'This action adds a new auth';
+
+	constructor(@InjectModel(User.name) private userModel: Model<User>) { }
+
+	async create(createUserDto: CreateUserDto): Promise<User> {
+		try {
+
+			const { password, ...userData } = createUserDto;
+			const newUser = new this.userModel({
+				password: bcryptjs.hashSync(password, 10),
+				...userData
+			});
+			await newUser.save();
+			const { password: _, ...user } = newUser.toJSON();
+			return user;
+		} catch (error) {
+			if (error.code === 1100) {
+				throw new BadRequestException(`${createUserDto.email} already exists!`);
+			}
+			throw new InternalServerErrorException('Something terrible happen!!');
+		}
+	}
+
+
+	async login(loginDto: LoginDto) {
+
+		const { email, password } = loginDto;
+		const user = await this.userModel.findOne({ email: email });
+		if (!user) {
+			throw new UnauthorizedException('Not valid credentials');
+		}
+		if (!bcryptjs.compareSync(password, user.password)) {
+			throw new UnauthorizedException('Not valid credentials');
+		}
+
+		const { password: _, ...rest } = user.toJSON();
+
+		return {
+			user: rest,
+			token: 'abc'
+		};
 	}
 
 	findAll() {
